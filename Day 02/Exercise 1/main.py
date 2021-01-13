77bc3e532d2084f54e5d00cdc21a581ae76285ee
Faranheit15 <ffaranm15@gmail.com>
2021-01-13 20:19:12 +0530

    start day 2

---
+++ Day 02/Exercise 1/main.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+two_digit_number_string=str(two_digit_number)
+print(int(two_digit_number_string[0])+int(two_digit_number_string[1]))
