cb3f9132c29c7ec1f074d5c21821f7c483e70a53
Faranheit15 <ffaranm15@gmail.com>
2021-01-13 20:43:05 +0530

    complete day 2 exercise 3

---
+++ Day 02/Exercise 3/main.py
@@ -0,0 +1,8 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+remainingAge=90-int(age)
+
+print(f"You have {remainingAge*365} days, {remainingAge*52} weeks, and {remainingAge*12} months left.")
