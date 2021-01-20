1f0d9e00bda3108f415351557d0a36385131a9d2
Faranheit15 <ffaranm15@gmail.com>
2021-01-20 20:24:25 +0530

    day 8 start

---
+++ Day 08/Exercise_1.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+def greet():
+  print("Hi")
+  print("Hello")
+  print("What's up")
+
+greet()
+
+def greet_with_name(name):
+  print(f"Hi {name}")
+  print(f"Hello {name}")
+  print(f"What's up {name}")
+
+greet_with_name("Faran")
