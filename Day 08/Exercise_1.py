2e1d881fa00868eed7cb5a6936f56ea9c995d265
Faranheit15 <ffaranm15@gmail.com>
2021-01-20 20:32:35 +0530

    day 8 exercise part 2

--- Day 08/Exercise_1.py
+++ Day 08/Exercise_1.py
@@ -15,3 +15,10 @@ def greet_with_name(name):
   print(f"What's up {name}")
 
 greet_with_name("Faran")
+
+def greet_with(name2,location):
+  print(f"Hello {name2}")
+  print(f"What is it like in {location}?")
+
+greet_with("Faran","Gorakhpur")
+greet_with(location="Valorant",name2="Phoenix")
