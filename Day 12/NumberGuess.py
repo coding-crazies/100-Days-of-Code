d0d8a5c3ca9bb98d4378856ac73516746fa1e45f
Faranheit15 <ffaranm15@gmail.com>
2021-01-31 20:50:51 +0530

    Day 12 Project Number Guessing game complete

--- Day 12/NumberGuess.py
+++ Day 12/NumberGuess.py
@@ -1,21 +1,49 @@
 from art import logo
 import random
 
-def check_answer(guess,answer):
-    if guess==answer:
-        return True
-    return False
+EASY_LEVEL_TURNS=10
+HARD_LEVEL_TURNS=5
 
-print(logo)
-print("Welcome to the Number Guessing Game!")
-print("I am thinking of a number between 1 and 100")
-answer=random.randint(1,100)
-print(f"Psst! The number is {answer}")
-level=input("Choose a difficulty 'easy' or 'hard': ")
-# if level=='easy':
-#     steps=10
-# elif level=='hard':
-#     steps=5
-# else:
-#     print("Wrong input!")
-guess=int(input("Make a guess: "))
+def check_answer(guess,answer,turns):
+    if guess>answer:
+        print("Too high!")
+        return turns-1
+    elif guess<answer:
+        print("Too low!")
+        return turns-1
+    else:
+        turns=0
+        print(f"You got it, the answer was {answer}")
+
+def set_difficulty():
+    level=input("Choose a difficulty 'easy' or 'hard': ")
+    if level=='easy':
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+def game():
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I am thinking of a number between 1 and 100")
+    answer=random.randint(1,100)
+    print(f"Psst! The number is {answer}")
+
+    turns=set_difficulty()
+    guess=0
+    while guess!=answer:
+        print(f"You have {turns} attempts remaining to guess the number!")
+
+        guess=int(input("Make a guess: "))
+
+        turns=check_answer(guess,answer,turns)
+
+        # turns-=1
+
+        if turns==0:
+            print("You ran out of attempts, you lose!")
+            return
+        elif guess!=answer:
+            print("Guess again!")
+
+game()
