7fc3847a61a1705502ca0cd7ad71137061355db4
Faranheit15 <ffaranm15@gmail.com>
2021-02-09 17:10:10 +0530

    turtle complete

--- Day 16/OOPs1.py
+++ Day 16/OOPs1.py
@@ -2,6 +2,9 @@ from turtle import Turtle, Screen
 
 kachua=Turtle()
 print(kachua)
+kachua.shape("turtle")
+kachua.color("green")
+kachua.forward(100)
 
 my_screen=Screen()
 print(my_screen.canvheight)
